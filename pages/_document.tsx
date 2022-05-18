711a499c27b1a228cdb7001066011a1940f02351
dex1t <dex1t@deg.ooo>
2022-05-18 15:02:39 +0900

    Remove unused components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Trykker&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <meta name="description" content="Pinch of Spice LLC." />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content="RINDA | Craft Vinegar Drink" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <body>
