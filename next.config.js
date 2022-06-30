8b6460e6943d9de7bf99b3e4618523b9bf91fd76
dex1t <dex1t@deg.ooo>
2022-06-30 18:57:40 +0900

    Add GA ID

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    gaId: phase === PHASE_DEVELOPMENT_SERVER ? "G-C0SQXQ1NEQ" : "G-Y91VZFYFX2",
+  },
 };
 
 module.exports = nextConfig;
